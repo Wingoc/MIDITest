217b72808afca9fe7b3a126889d379c92b9dd5fd
wing <741066288@qq.com>
2018-01-03 15:36:50 +0800

    first commit

---
+++ resources/main.js
@@ -0,0 +1,576 @@
+
+$(function(){
+
+	// header 
+	var connect = $("#connect");
+	var currentDev = $("#currentDev");
+	var audio = $("#audio");
+	var refresh = $("#refresh");
+	var deviceId = 0;
+
+
+	// Output Section
+	var noteoff = $("#noteoff");
+	var noteon = $("#noteon");
+	var serial = $("#serial");
+	var continuity = $("#continuity");
+	var message = $("#message");
+	var send = $("#send");
+	var sendLoop = $("#sendLoop");
+	var messageLoop = $("#messageLoop");
+
+	const singleMidi = ["0xF6", "0xF8", "0xFA", "0xFB", "0xFC", "0xFE", "0xFF"];
+
+	// Input Section
+	var clear = $("#clear");
+	var reset = $("#reset");
+	var Noteon_show = $("#Noteon_show");
+	var Noteoff_show = $("#Noteoff_show");
+	var An_show = $("#An_show");
+	var Bn_show = $("#Bn_show");
+	var Cn_show = $("#Cn_show");
+	var Dn_show = $("#Dn_show");
+	var En_show = $("#En_show");
+	var Sysex_show = $("#Sysex_show");
+	var show = $("#show");
+
+
+	// 计数
+	var all_noteoff = 0;
+	var all_noteon = 0;
+	var all_keyaftertouch = 0;
+	var all_controlb = 0;
+	var all_programchange = 0;
+	var all_channelaftertouch = 0;
+	var all_pitchbend = 0;
+	var all_sysex = 0;
+
+	var clickTime = 0;
+	var stop = "";
+
+	
+
+
+	connect.click(function(){
+
+		WebMidi.enable(function (err) {
+			if (err) {
+				alert("Sorry! Please make sure your MIDI device is connected to the computer!");
+			} else {
+				console.log(WebMidi.sysexEnabled);
+
+				if ($("#devices").children().length == 0) {
+					for (var i = 0; i < WebMidi.outputs.length; i++) {
+
+						var $li = $("<li></li>");
+						var $btn = $("<button>" + WebMidi.outputs[i].name + "</button>");
+						$btn.attr({
+							"id": ""+i,
+							"class": "btn btn-info col-md-12"
+						});
+						$li.append($btn);
+						$("#devices").append($li);
+						$btn.click(function(){
+							deviceId = $(this).attr("id");  // this指向绑定的元素
+
+							var output = WebMidi.outputs[deviceId];
+							var input = WebMidi.inputs[deviceId];				
+
+							var devName = output.name;
+							var current = $("<strong>" + devName + "<strong>");
+							current.css("color", "green");
+							currentDev.text(" ");
+							currentDev.append(current);
+
+
+							// Note Off Listener
+							input.addListener('noteoff', "all", 
+								function (e) {
+									all_noteoff++;
+									Noteoff_show.text(all_noteoff);
+
+									var color = "#009797";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// Note On Listener
+							input.addListener('noteon', "all", 
+								function (e) {
+									all_noteon++;
+									Noteon_show.text(all_noteon);
+									var color = "#06ff06";
+
+									var raw = e.data;
+									var processed = RawDataProcessing(raw); 									  
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// Key After Touch Listener
+							input.addListener('keyaftertouch', "all", 
+								function (e) {
+									all_keyaftertouch++;
+									An_show.text(all_keyaftertouch);
+
+									var color = "#32D6DA";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw); 					  
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+
+							// Controller Listener
+							input.addListener('controlchange', "all", 
+								function (e) {
+									all_controlb++;
+									Bn_show.text(all_controlb);
+
+									var color = "#0BA4FF";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// Controller Listener
+							input.addListener('channelmode', "all", 
+								function (e) {
+									all_controlb++;
+									Bn_show.text(all_controlb);
+
+									var color = "#0BA4FF";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// Program Change Listener
+							input.addListener('programchange', "all", 
+								function (e) {
+									all_programchange++;
+									Cn_show.text(all_programchange);
+
+									var color = "#B543CC";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// Channel After Listener
+							input.addListener('channelaftertouch', "all", 
+								function (e) {
+									all_channelaftertouch++;
+									Dn_show.text(all_channelaftertouch);
+
+									var color = "#32D6DA";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// Pitchbend Listener
+							input.addListener('pitchbend', "all", 
+								function (e) {
+									all_pitchbend++;
+									En_show.text(all_pitchbend);
+
+									var color = "#FF0683";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// Sysex Listener
+							input.addListener('sysex', "all", 
+								function (e) {
+									all_sysex++;
+									Sysex_show.text(all_sysex);
+
+									var color = "#FFFF8E";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// F1 Listener
+							input.addListener('timecode', "all", 
+								function (e) {
+
+									var color = "red";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// F2 Listener
+							input.addListener('songposition', "all", 
+								function (e) {
+
+									var color = "red";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// F3 Listener
+							input.addListener('songselect', "all", 
+								function (e) {
+
+									var color = "red";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// F6 Listener
+							input.addListener("tuningrequest", "all", 
+								function (e) {
+
+									var color = "red";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// F8 Listener
+							input.addListener('clock', "all", 
+								function (e) {
+
+									var color = "red";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// FA Listener
+							input.addListener('start', "all", 
+								function (e) {
+
+									var color = "red";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+									show[0].scrollTop = show[0].scrollHeight;
+
+									AutoClear();
+								});
+
+							// FB Listener
+							input.addListener('continue', "all", 
+								function (e) {
+
+									var color = "red";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// FC Listener
+							input.addListener('stop', "all", 
+								function (e) {
+
+									var color = "red";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// FE Listener
+							input.addListener('activesensing', "all", 
+								function (e) {
+
+									var color = "red";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+							// FF Listener
+							input.addListener('reset', "all", 
+								function (e) {
+
+									var color = "red";
+									var raw = e.data;
+
+									var processed = RawDataProcessing(raw);   
+									createElement(devName, processed, color);
+
+									show[0].scrollTop = show[0].scrollHeight;
+									AutoClear();
+								});
+
+
+
+
+							/********* Output Section ********/
+							// Note On C3
+							noteon.click(function(){
+								output.playNote("C3", 1, {velocity: 1});
+							});	
+
+							// Note Off C3
+							noteoff.click(function(){
+								output.stopNote("C3", 1, {velocity: 1});
+							});	
+
+							// Serial Number Query
+							serial.click(function(e){
+								// output.sendSysex([],[0x00,0x20,0x2b,0x69,0x01,0x02]);
+
+								output.send(0x80, [0x11, 0x22]);
+								output.send(0x90, [0x11, 0x22]);
+								output.send(0xA0, [0x11, 0x22]);
+								output.send(0xC0, [0x11]);
+								output.send(0xF0, [0x11, 0x22, 0xF7]);
+
+							});
+
+							// Note On C3 continuity 200ms/time
+							continuity.click(function(){
+								if (clickTime % 2 === 0) {
+									clickTime++;
+									continuity.html("<strong>Stop</strong>");
+									continuity.css("color", "red");
+									stop = setInterval(function(){
+										output.playNote("C3", 1, {velocity: 1});
+									}, 200);
+								} else {
+									clickTime++;
+									clearInterval(stop);
+									continuity.html("<strong>Noto On C3 Continuity</strong>");
+									continuity.css("color", "white");
+								}
+							});
+
+							// Just send
+							send.click(function(){
+
+								var data = message.val();
+								if (data === "") {
+									alert("The input value can not be empty!");
+								} else {
+									var dataArr = data.split(" ");
+									console.log(dataArr);
+									var statusArr = [];
+									var midiData = [];
+									var status = "";
+
+									dataArr.forEach(function(value, index, array){
+										var item = "0x" + value.toUpperCase();
+
+										if (singleMidi.indexOf(item) >= 0){
+											statusArr.push(item);
+											status = statusArr.shift();
+											console.log(status);
+											console.log(midiData);
+											output.send(status, midiData);    // empty midiData
+											midiData.splice(0,midiData.length);
+											} else if (item > 127 && item < 256 && item != 247) {
+												statusArr.push(item);
+												if (statusArr.length > 1 || index == (array.length-1)) {
+													status = statusArr.shift();
+													console.log(status);
+													console.log(midiData);
+													output.send(status, midiData);
+													midiData.splice(0,midiData.length);    // empty midiData
+												}
+											} else if (item < 128 || item == 247) {
+												midiData.push(item);									
+												if (index == (array.length-1)) {      // is the last element of array
+													status = statusArr.shift();
+													console.log(status);
+													console.log(midiData);	
+													output.send(status, midiData);
+												}
+											}
+										});
+								}
+							});
+							
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+						});
+					}
+				}
+			}
+		}, true);		
+	});
+
+
+
+	
+
+
+
+
+
+	function NoteOnC3(){
+
+	}
+
+
+
+
+
+
+
+
+
+
+	// return Array
+	function RawDataProcessing(data) {
+
+		var tempArr = [];
+		for (var i = 0; i < data.length; i++) {
+			var str = data[i].toString(16).toUpperCase();
+			if (str.length < 2) {
+				str = "0" + str;
+				tempArr.push(str);
+			} else {
+				tempArr.push(str);
+			}
+		}
+		return tempArr;
+	}
+
+	// return none
+	function createElement(name, arr, color){
+
+		var timestamp = (new Date()).valueOf().toString();
+		var message = (timestamp + "&emsp;&emsp;&emsp;" + name + "&emsp;&emsp;&emsp;" + arr.join("&emsp;"));
+		var elemet = $("<span>" + message + "</span>");
+		elemet.css("color", color);
+		var br = $("<br>");
+		show.append(elemet);
+		show.append(br);
+	}
+
+	// MIDI Message > 200 and SysEx Message will auto clear the Input Section
+	function AutoClear() {
+
+		var sum = all_noteon + all_noteoff + all_keyaftertouch + all_controlb + all_pitchbend + all_channelaftertouch + all_programchange;
+		if ((sum+2)%502 == 0 || (all_sysex+1)%20 == 0) {
+			Clear();
+		}
+	}
+
+	// Refresh current page
+	refresh.click(function(){
+		window.location.reload();
+	});
+
+	// Reset calculation
+	reset.click(function(){
+		
+		all_noteoff = 0;
+		all_noteon = 0;	
+		all_keyaftertouch = 0;	
+		all_controlb = 0;	
+		all_programchange = 0;	
+		all_channelaftertouch = 0;	
+		all_pitchbend = 0;		
+		all_sysex = 0;	
+
+		Noteoff_show.text(0);
+		Noteon_show.text(0);
+		An_show.text(0);
+		Bn_show.text(0);
+		Cn_show.text(0);
+		Dn_show.text(0);
+		En_show.text(0);
+		Sysex_show.text(0);
+	});
+
+	// Clear Input Section
+	clear.click(function(){
+		Clear();
+	});	
+
+	// Clear shown message
+	function Clear(){
+		show.text(" ");
+	}
+
+});
+
